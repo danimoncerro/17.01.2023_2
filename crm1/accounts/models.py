dfd36cd368d444c4b45d011dc2517323d9772c81
daniel <dani_moncerro@yahoo.com>
2023-01-25 03:02:02 +0200

    25.01.2023

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -44,6 +44,7 @@ class Order(models.Model):
     #product =
     date_created =models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.product.name
