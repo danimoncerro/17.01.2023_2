3582f3bbf241831a538816c990559439fa11077f
daniel <dani_moncerro@yahoo.com>
2023-01-25 23:34:49 +0200

    25.01.2023

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -25,6 +25,7 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user,)
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
     context = {'form': form}
@@ -64,9 +65,23 @@ def home(request):
                 'pending': pending}
     return render(request, 'accounts/dashboard.html', context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered,
+                'pending': pending}
     return render(request, 'accounts/user.html', context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def accountSettings(request):
+    context = {}
+    return render(request, 'accounts/account_settings.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
