b380158ac14a11104c45d15df2b21c755509cf96
danimoncerro <dani_moncerro@yahoo.com>
2023-01-26 18:09:56 +0200

    26.01.2023a

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -52,4 +53,29 @@ class Order(models.Model):
     def __str__(self):
         return self.product.name
 
-  
+'''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return str(self.user)
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        print('Prfile created')
+
+post_save.connect(create_profile, sender=User)
+
+def update_profile(sender, instance, created, **kwargs):
+    if created == False:
+        instance.profile.save()
+        print('Profile updated')
+        print()
+
+    
+post_save.connect(update_profile, sender=User)
+'''
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -8,7 +8,6 @@ from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
 
@@ -23,9 +22,6 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,)
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
     context = {'form': form}
