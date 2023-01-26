89a8427384b0ec6f0922fab93fe90948cd471732
daniel <dani_moncerro@yahoo.com>
2023-01-27 01:36:11 +0200

    test

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -131,3 +131,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+
+'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.yahoo.com' # 'smtp.gmail.com' 
+EMAIL_PORT = 465 #587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dani_moncerro@yahoo.com'
+EMAIL_HOST_PASSWORD = 'PetraAngela3'
+'''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'danielmoncerro@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sebidanp2'
